e7c1e106c66e8963d1c9a15a626bc81d6f3e42d0
tkopacz <tkopacz@tomaszkopacz.com>
2016-02-26 15:59:17 +0100

    Work

--- Test4_TCS34725/Test4_TCS34725/MainPage.xaml.cs
+++ Test4_TCS34725/Test4_TCS34725/MainPage.xaml.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Devices.Adc;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -14,12 +14,9 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-//Download ADC Providers from: https://github.com/ms-iot/BusProviders.git
-using Microsoft.IoT.AdcMcp3008;
-using System.Diagnostics;
-
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-namespace Test3_MCP3008
+
+namespace Test4_TCS34725
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -31,31 +28,23 @@ namespace Test3_MCP3008
             this.InitializeComponent();
             setup();
         }
-
-        private AdcController m_adc;
-        private AdcChannel[] m_adcChannel;
+        TCS34725 m_tcs;
         DispatcherTimer m_t;
+
         private async void setup()
         {
-            m_adc = (await AdcController.GetControllersAsync(AdcMcp3008Provider.GetAdcProvider()))[0];
-            m_adcChannel = new AdcChannel[m_adc.ChannelCount];
-            for (int i = 0; i < m_adc.ChannelCount; i++)
-            {
-                m_adcChannel[i] = m_adc.OpenChannel(i);
-            }
+            m_tcs = new TCS34725();
+            await m_tcs.Initialize();
             m_t = new DispatcherTimer();
-            m_t.Interval = TimeSpan.FromSeconds(5);
+            m_t.Interval = TimeSpan.FromSeconds(1);
             m_t.Tick += M_t_Tick;
             m_t.Start();
         }
 
-        private void M_t_Tick(object sender, object e)
+        private async void M_t_Tick(object sender, object e)
         {
-            for (int i = 0; i < m_adc.ChannelCount; i++)
-            {
-                Debug.Write($"{i}:{m_adcChannel[i].ReadRatio():F4}, ");
-            }
-            Debug.WriteLine("");
+            var color = await m_tcs.GetRgbDataAsync();
+            Debug.WriteLine($"{color.Red},{color.Green},{color.Blue}");
         }
     }
 }
